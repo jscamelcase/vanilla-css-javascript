38e572f96e8982496c8569d1e777234920e4b39b
Carl Paraskevas <carl.paraskevas@gmail.com>
2024-12-16 17:16:18 +0000

    added movie screen graphic

--- form-validator/script.js
+++ form-validator/script.js
@@ -1,3 +1,13 @@
+/* 
+
+- basic refactoring
+- refactoring into functions for code generalisation 
+- string manipulation
+- regex testing
+- classList manipulation for conditonal UI changes
+
+*/
+
 const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
