cd648b5a20195e90d68a0cabe02d913f517e05a3
Carl Paraskevas <carl.paraskevas@gmail.com>
2024-12-16 19:53:32 +0000

    populate UI With Saved Data

--- movie-booking/script.js
+++ movie-booking/script.js
@@ -7,6 +7,8 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 
 // Save selected movie index and price
@@ -35,6 +37,26 @@ function updateSelectedCount() {
   total.textContent = selectedSeatsCount * ticketPrice;
 }
 
+//Get data from localstorage and populate UI
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 //Movie select event
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
@@ -53,3 +75,6 @@ container.addEventListener("click", (e) => {
     updateSelectedCount();
   }
 });
+
+// Initl count and total set
+updateSelectedCount();
