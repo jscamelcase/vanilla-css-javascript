60af2fbefbfc8e4d235db15e158a84edeeba7e7d
Carl Paraskevas <carl.paraskevas@gmail.com>
2024-12-17 17:05:01 +0000

    updated video player notes

--- custom-video-player/script.js
+++ custom-video-player/script.js
@@ -1,9 +1,59 @@
+/* This project focuses on using the html video element api 
+to customise a video player */
+
 //obtain the necessary DOM elements
 const video = document.getElementById("video");
 const play = document.getElementById("play");
 const stopPlay = document.getElementById("stop");
 const progress = document.getElementById("progress");
 const timestamp = document.getElementById("timestamp");
+
+//Play & Pause Video
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+//Update play/pause icon
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+}
+
+//Update progress & timestamp
+function updateProgress() {
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  //Get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = "0" + String(mins);
+  }
+
+  //Get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = "0" + String(secs);
+  }
+  timestamp.innerHTML = `${mins}:${secs}`;
+}
+
+//Set video time to progress
+function setVideoProgress() {
+  video.currentTime = (+progress.value * video.duration) / 100;
+}
+
+//Stop video
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+}
 
 //Event listeners
 video.addEventListener("click", toggleVideoStatus);
@@ -13,6 +63,6 @@ video.addEventListener("timeupdate", updateProgress);
 
 play.addEventListener("click", toggleVideoStatus);
 
-stopPlay.addEventListener("click", stopvideo);
+stopPlay.addEventListener("click", stopVideo);
 
-progress.addEventListener("change", setvideoProgress);
+progress.addEventListener("change", setVideoProgress);
