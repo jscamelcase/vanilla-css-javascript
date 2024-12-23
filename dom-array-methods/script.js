7f6437193e4950c58fc9fedbbdd5ab5ccd43cf33
Carl Paraskevas <carl.paraskevas@gmail.com>
2024-12-23 10:46:03 +0000

    menu-modal intial commit

--- dom-array-methods/script.js
+++ dom-array-methods/script.js
@@ -36,7 +36,6 @@ function addData(obj) {
 
 // Double everyones money
 function doubleMoney() {
-  console.log("from doubleMoney");
   data = data.map((user) => {
     return { ...user, money: user.money * 2 };
   });
@@ -53,6 +52,17 @@ function sortByRichest() {
 function showMillionaires() {
   data = data.filter((user) => user.money > 1000000);
   updateDOM();
+}
+
+// Calculate the total wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement("div");
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )}</strong></h3>`;
+  main.appendChild(wealthEl);
 }
 
 //Update DOM
@@ -82,3 +92,4 @@ addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
 sortBtn.addEventListener("click", sortByRichest);
 showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
